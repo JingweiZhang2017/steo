109ef4a8a8f9110c67fa84844c72249ec5fb8f84
Jingwei <jingwei.zhang@refinitiv.com>
2021-05-10 02:04:56 +0100

    Add evaluation functions

--- STEO.py
+++ STEO.py
@@ -6,7 +6,11 @@ import datetime
 import logging
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 import pandas as pd
-
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+import numpy as np
+np.seterr(divide='ignore', invalid='ignore')
+from math import sqrt
 
 # Series of Interest 
 code_dict = [{'code':'WTIPUUS','description':'West Texas Intermediate Spot Average', 'category': 'Crude oil'},
@@ -96,19 +100,19 @@ def extract_target_series_since0710(path, file_name):
     Target series are series of interest defined in the dictionary code_dict (such as 'WTIPUUS')
     
     Input:       string file names like '0710.xls' 
-    Output:      a dictionary with KEYs as codes of price of interest and VALUEs as price value series indexed by time codes ('0810' means 'October of 2010'), the example is shown as follow:
+    Output:      a dictionary with KEYs as codes of price of interest and VALUEs as price value series indexed by time codes ('200810' means 'October of 2008'), the example is shown as follow:
   {'WTIPUUS':
-    0301    32.96
-    0302    35.83
-    0303    33.51
-    0304    28.17
-    0305    28.11
+    200301    32.96
+    200302    35.83
+    200303    33.51
+    200304    28.17
+    200305    28.11
             ...  
-    0808       72
-    0809       73
-    0810       72
-    0811     71.5
-    0812       71,
+    200808       72
+    200809       73
+    200810       72
+    200811     71.5
+    200812       71,
    'NGHHMCF':
             ...
     }
@@ -171,7 +175,7 @@ def get_pred_matrix (series_df, target_price, end_year, start_year = '200810'):
     matrix_df.columns = ['actual'] + ['pred_%smonth'%str(i+1) for i in range(12)]
     return matrix_df
 
-def transform_data(data_path, output_path):
+def transform_data(data_path, output_path, save = True):
     file_name_list = sorted([file for file in os.listdir(data_path) if file.split('.')[1].startswith('xls') and int(file.split('.')[0]) >= int('200710')])
     price_dict_list = list()
     pct_dict_list = list()
@@ -187,13 +191,70 @@ def transform_data(data_path, output_path):
     for target_price in target_df.code.tolist():
         price_matrix_dict[target_price] = get_pred_matrix(price_series_df, target_price, end_year = file_name_list[-2].split('.')[0])
         pct_matrix_dict[target_price] = get_pred_matrix(pct_series_df, target_price, end_year = file_name_list[-2].split('.')[0])
-        
-        price_matrix_dict[target_price].to_csv(output_path + '/price_%s.csv'%target_price)
-        print('price_%s.csv is created'%target_price)
-        pct_matrix_dict[target_price].to_csv(output_path + '/pct_%s.csv'%target_price)
-        print('pct_%s.csv is created'%target_price)
+        if save == True:
+            price_matrix_dict[target_price].to_csv(output_path + '/price_%s.csv'%target_price)
+            print('price_%s.csv is created'%target_price)
+            pct_matrix_dict[target_price].to_csv(output_path + '/pct_%s.csv'%target_price)
+            print('pct_%s.csv is created'%target_price)
     return price_matrix_dict, pct_matrix_dict
-              
+
+#   Forecast Evaluation  
+def get_evaluation(matrix_dict, eval_path, dict_type):
+    '''
+    dict_type: 'price' (US dollar forecasts) or 'pct' (pct change forecasts)
+    '''
+    
+    def mean_absolute_percentage_error(y_true, y_pred): 
+        y_true, y_pred = np.array(y_true), np.array(y_pred)
+        return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+    def absolute_errors(y_true, y_pred): 
+        y_true, y_pred = np.array(y_true), np.array(y_pred)
+        return np.abs(y_true - y_pred)
+    
+    def get_metric_df(metric_series):
+        df = pd.DataFrame(metric_series, columns = pred_cols)
+        df.index = target_df.code.tolist()
+        return df
+    
+    mae = list()
+    rmse = list()
+    mape = list()
+    ae_dict = dict()
+    ape_dict = dict()
+    actual_list = list()
+    for target_price in target_df.code.tolist():
+        matrix_df = matrix_dict[target_price]
+        y_true = matrix_df['actual']
+        actual_list.append(y_true.rename(target_price))
+        pred_cols = matrix_df.columns.tolist()[1:]
+        t_mae = list()
+        t_rmse = list()
+        t_mape = list()
+        ae_df = pd.DataFrame()
+        ape_df = pd.DataFrame()
+        for pred_col in pred_cols:
+            y_pred = matrix_df[pred_col]
+            t_rmse.append(sqrt(mean_squared_error(y_true, y_pred)))
+            t_mae.append(mean_absolute_error(y_true, y_pred))
+            t_mape.append(mean_absolute_percentage_error(y_true, y_pred))
+            ae_df[pred_col.split('_')[1]] = absolute_errors(y_true, y_pred)
+            ape_df[pred_col.split('_')[1]] = absolute_errors(y_true, y_pred)/y_true
+        ae_dict[target_price] = ae_df 
+        ape_dict[target_price] = ape_df 
+        mae.append(t_mae)
+        rmse.append(t_rmse)
+        mape.append(t_mape)
+    actual_df = pd.DataFrame(actual_list)
+    mae_df = get_metric_df(mae)
+    rmse_df = get_metric_df(rmse)
+    mae_df.to_csv(eval_path + '/mae_%s.csv'%dict_type)
+    rmse_df.to_csv(eval_path + '/rmse_%s.csv'%dict_type)
+    if dict_type == 'price':
+        get_metric_df(mape).to_csv(eval_path + '/mape_%s.csv'%dict_type)
+    return actual_df, ae_dict, ape_dict
+
+
 def main(argv):
     # create a data directory in your local
     paths = [os.getcwd() + path for path in ['/data','/pred','/eval']]
@@ -206,7 +267,11 @@ def main(argv):
     # download excel tables to the defined directory
     download_excel_data(outdir = paths[0])
     logging.info('TRANSFORMING tables into X-month ahead forecasts under %s'%paths[1])
+    # for each series of interest, transform the forecasts into matrix with rows denoting the reference period while the columns denoting the relevant forecast publication period (vintage).
     price_matrix_dict, pct_matrix_dict = transform_data(paths[0],paths[1])
+    # create evaluations in metrics like mae, rmse and mape
+    price_actual_df, price_ae_dict, price_ape_dict = get_eval(price_matrix_dict, paths[2], 'price')
+    pct_actual_df, pct_ae_dict, pct_ape_dict = get_eval(pct_matrix_dict, paths[2], 'pct')
         
     
 
